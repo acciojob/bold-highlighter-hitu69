83089a361b456fe7a66f4e6ae5151ec26489b745
Hitesh Marwaha <73687191+hitu69@users.noreply.github.com>
2023-04-14 11:34:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,6 @@
   }
 
  }
- para1.addEventListener("mouseout",return_normal)
  function return_normal(){
      var boldWords = document.querySelectorAll('strong');
   for (var i = 0; i < boldWords.length; i++) {
